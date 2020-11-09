703eaae00e46d9c86040ac79d24c09d8818b5b2b
Sarah Habershon <habershon@gmail.com>
2020-11-09 16:10:07 +0100

    still a problem with the merge not capturing all dates on left join

--- match_all.py
+++ match_all.py
@@ -1,30 +1,37 @@
 import pandas as pd 
 
 eur_risk_strin = pd.read_csv("europe_joined.csv", parse_dates =["Date"])
-goog_mob = pd.read_csv("mobility_europe.csv",)
-cases = pd.read_csv("europeancases.csv")
+goog_mob = pd.read_csv("mobility_europe.csv", parse_dates =["Date"])
+cases = pd.read_csv("europeancases.csv", parse_dates =["Date"])
 
 #dates to datetime
 goog_mob['Date'] = pd.to_datetime(goog_mob['Date'], format='%Y-%m-%d')
 eur_risk_strin['Date'] = pd.to_datetime(eur_risk_strin['Date'], format='%Y-%m-%d')
 cases['Date'] = pd.to_datetime(cases['Date'], format='%Y-%m-%d')
 
-
-join_one = pd.merge(eur_risk_strin,
-                goog_mob,
-                on=["Date", "CountryCode"],
-                how = "inner")
-
-
-print(join_one.head)
-
-join_one.to_csv("mobility_stringency_match.csv")
+join_one = pd.merge(cases,
+                    eur_risk_strin,
+                    on=["Date", "CountryCode"],
+                    how = "left")
 
 join_two = pd.merge(join_one,
-            cases,
-            on = ["Date", "CountryCode"],
-            how = "inner")
+                    goog_mob,
+                    on=["Date", "CountryCode"],
+                    how = "left")
+
+# join_one = pd.merge(eur_risk_strin,
+#                 goog_mob,
+#                 on=["Date", "CountryCode"],
+#                 how = "inner")
+
+
+# join_one.to_csv("mobility_stringency_match.csv")
+
+# join_two = pd.merge(join_one,
+#             cases,
+#             on = ["Date", "CountryCode"],
+#             how = "inner")
 
 
 
-join_two.to_csv("total_merge.csv")
+join_two.to_csv("total_merge_version_two.csv")
--- mergeplot.py
+++ mergeplot.py
@@ -2,31 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv("mobility_stringency_match.csv", parse_dates =["Date"])
+data = pd.read_csv("total_merge_version_two.csv", parse_dates =["Date"])
 
 data.groupby("name").plot
 
+print(data.columns)
 
 def makeCharts(x):
     plt.clf()
     country = data.loc[data['name'] == str(x)]
     name = country["name"]
     country = data.loc[((data['name'] == x) & (data['RegionCode'].isnull()))]
-    country['openness_risk'] = country['openness_risk']*100
+    country['StringencyIndex'] = country['StringencyIndex']/100
     plt.plot("Date", "openness_risk", data=country, marker='', linewidth=2)
     plt.plot("Date", "StringencyIndex", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "new_cases_index", data=country, linewidth=2)
+    plt.plot("Date", "rolling_cases_index", data=country, linewidth=2)
     # plt.plot("Date", "retail_and_recreation_percent_change_from_baseline", data=country, marker='', linewidth=2)
-    plt.plot("Date", "rolling_avg_retail_and_recreation", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "rolling_avg_retail_and_recreation", data=country, marker='', linewidth=2)
     # # # plt.plot("Date", "workplaces_percent_change_from_baseline", data=country, marker='', linewidth=2)
-    plt.plot("Date", "rolling_avg_workplaces", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "rolling_avg_workplaces", data=country, marker='', linewidth=2)
     # # # plt.plot("Date", "grocery_and_pharmacy_percent_change_from_baseline", data=country, marker='', linewidth=2)
-    plt.plot("Date", "rolling_avg_grocery_and_pharmacy", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "rolling_avg_grocery_and_pharmacy", data=country, marker='', linewidth=2)
     # # # plt.plot("Date", "parks_percent_change_from_baseline", data=country, marker='', linewidth=2)
     # plt.plot("Date", "rolling_avg_parks", data=country, marker='', linewidth=2)
     # # # plt.plot("Date", "transit_stations_percent_change_from_baseline", data=country, marker='', linewidth=2)
-    plt.plot("Date", "rolling_avg_transit_stations", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "rolling_avg_transit_stations", data=country, marker='', linewidth=2)
     # # # plt.plot("Date", "residential_percent_change_from_baseline", data=country, marker='', linewidth=2)
-    plt.plot("Date", "rolling_avg_residential", data=country, marker='', linewidth=2)
+    # plt.plot("Date", "rolling_avg_residential", data=country, marker='', linewidth=2)
     plt.title(str(x))
     plt.legend()
     plt.show()
